58595bc12399ef737624d2191c752f6bc3872043
Harvey Fong <harvey@oomer.org>
2024-08-09 19:22:24 -0600

    added support for non admin user, limit pgrep to current user group

--- cloud_bella_cli.sh
+++ cloud_bella_cli.sh
@@ -6,8 +6,8 @@ image_ext="png"
 systemd="1"
 bsa64=""
 anim64=""
-#cat render.sh | base64
-render_sh="IyEvYmluL2Jhc2gKb3NfbmFtZT0kKGF3ayAtRj0gJyQxPT0iTkFNRSIgeyBwcmludCAkMiA7fScgL2V0Yy9vcy1yZWxlYXNlKQpwbGF0Zm9ybV9pZD0kKGF3ayAtRj0gJyQxPT0iUExBVEZPUk1fSUQiIHsgcHJpbnQgJDIgO30nIC9ldGMvb3MtcmVsZWFzZSkKdXNlcj0kKHdob2FtaSkKCmlmICEgdGVzdCAtZiBiZWxsYV9jbGk7IHRoZW4KCSMgUkhFTCA4LnggYW5kIDkueCAKCWlmIFsgIiRwbGF0Zm9ybV9pZCIgPT0gIlwicGxhdGZvcm06ZWw4XCIiIF0gfHwgWyAiJHBsYXRmb3JtX2lkIiA9PSAiXCJwbGF0Zm9ybTplbDlcIiIgXTsgdGhlbgoJCWlmIFsgJHtVU0VSfSA9PSAiYmVsbGEiIF07IHRoZW4KCQkJZWNobyAiYnlwYXNzIgoJCWVsaWYgWyAke1VTRVJ9ID09ICJyb290IiBdOyB0aGVuCgkJCWRuZiAteSBpbnN0YWxsIG1lc2EtdnVsa2FuLWRyaXZlcnMKCQkJZG5mIC15IGluc3RhbGwgbWVzYS1saWJHTAoJCQlkbmYgLXkgaW5zdGFsbCBiYwoJCWVsc2UKCQkJc3VkbyBkbmYgLXkgaW5zdGFsbCBtZXNhLXZ1bGthbi1kcml2ZXJzCgkJCXN1ZG8gZG5mIC15IGluc3RhbGwgbWVzYS1saWJHTAoJCQlzdWRvIGRuZiAteSBpbnN0YWxsIGJjCgkJZmkKCSMgIERlYmlhbiBiYXNlZAoJZWxzZQoJCWlmIFsgJHtVU0VSfSA9PSAiYmVsbGEiIF07IHRoZW4KCQkJZWNobyAiYnlwYXNzIgoJCWVsaWYgWyAke1VTRVJ9ID09ICJyb290IiBdOyB0aGVuCgkJCWFwdCAteSB1cGRhdGUKCQkJYXB0IC15IGluc3RhbGwgbWVzYS12dWxrYW4tZHJpdmVycwoJCQlhcHQgLXkgaW5zdGFsbCBsaWJnbDEtbWVzYS1nbHgKCQkJYXB0IC15IGluc3RhbGwgYmMKCQllbHNlCgkJCXN1ZG8gYXB0IC15IHVwZGF0ZQoJCQlzdWRvIGFwdCAteSBpbnN0YWxsIG1lc2EtdnVsa2FuLWRyaXZlcnMKCQkJc3VkbyBhcHQgLXkgaW5zdGFsbCBsaWJnbDEtbWVzYS1nbHgKCQkJc3VkbyBhcHQgLXkgaW5zdGFsbCBiYwoJCWZpCglmaQoJaWYgWyAteiAke0JFTExBX1ZFUlNJT059IF07IHRoZW4KCQljdXJsIC1PIGh0dHBzOi8vZG93bmxvYWRzLmJlbGxhcmVuZGVyLmNvbS9iZWxsYV9jbGktMjQuMi4wLnRhci5negoJCXRhciAteHZmIGJlbGxhX2NsaS0yNC4yLjAudGFyLmd6CgllbHNlCgkJY3VybCAtTyBodHRwczovL2Rvd25sb2Fkcy5iZWxsYXJlbmRlci5jb20vYmVsbGFfY2xpLSR7QkVMTEFfVkVSU0lPTn0udGFyLmd6CgkJdGFyIC14dmYgYmVsbGFfY2xpLSR7QkVMTEFfVkVSU0lPTn0udGFyLmd6CglmaQpmaQoKYnN6X2ZpbGVzPSouYnN6CgppZiAhIHRlc3QgLWYgImJlbGxhLmxvZyIgOyB0aGVuCglpZGxlPSIxIgplbHNlCglpc19pZGxlPSIkKHBncmVwIGJlbGxhX2NsaSkiCgoJaWYgWyAteiAke2lzX2lkbGV9IF07IHRoZW4KCQlpZGxlPSIxIgoJZWxzZQoJCWlkbGU9IjAiCglmaQpmaQoKCmlmIFsgJHtpZGxlfSA9PSAiMSIgXTsgdGhlbgoJZm9yIGVhY2ggaW4gJGJzel9maWxlcwoJZG8KCQlwcmVmaXg9JHtlYWNoJS4qfQoJCWVjaG8gIlJlbmRlcmluZyBzdGFydGVkIGZvcjogJGVhY2giCgkJaWYgdGVzdCAtZiBjbG91ZF9iZWxsYV9jbGkuYW5pbTsgdGhlbgoJCQlhbmltMXN0YXJ0bGluZT0kKHNlZCAnMSFkJyBjbG91ZF9iZWxsYV9jbGkuYW5pbSkKCQkJYXR0cj0ke2FuaW0xc3RhcnRsaW5lJT0qfQoJCQlhbmltMXN0YXJ0MD0ke2FuaW0xc3RhcnRsaW5lIyo9fQoJCQlhbmltMXN0YXJ0PSR7YW5pbTFzdGFydDA6MDokeyNhbmltMXN0YXJ0MH0tMn0KCQkJYW5pbTFlbmRsaW5lPSQoc2VkICcyIWQnIGNsb3VkX2JlbGxhX2NsaS5hbmltKQoJCQlhbmltMWVuZDA9JHthbmltMWVuZGxpbmUjKj19CgkJCWFuaW0xZW5kPSR7YW5pbTFlbmQwOjA6JHsjYW5pbTFlbmQwfS0yfQoJCQlmcmFtZXM9JChzZWQgJzMhZCcgY2xvdWRfYmVsbGFfY2xpLmFuaW0pCgkJCWZvciAoKGkgPSAwIDsgaSA8PSAke2ZyYW1lc30gOyBpKysgKSk7IGRvIAoJCQkJcGFkZGVkPSQocHJpbnRmICIlMDRkIiAkKChpKzEpKSkKCQkJCWVjaG8gInNjYWxlPTU7ICgke2FuaW0xZW5kfS0oJHthbmltMXN0YXJ0fSkpKigke2l9LyR7ZnJhbWVzfS4wKSske2FuaW0xc3RhcnR9IiB8IGJjCgkJCQlhbmltYXRlZD0kKGVjaG8gInNjYWxlPTU7ICgoJHthbmltMWVuZH0tKCR7YW5pbTFzdGFydH0pKSooJHtpfS8ke2ZyYW1lc30uMCkpKyR7YW5pbTFzdGFydH0iIHwgYmMpCgkJCQlpZiBbICR7YW5pbWF0ZWQ6MDoxfSA9PSAiLiIgXTsgdGhlbgoJCQkJCWFuaW1hdGVkPSIwJHthbmltYXRlZH0iCgkJCQllbGlmIFsgJHthbmltYXRlZDowOjJ9ID09ICItLiIgXTsgdGhlbgoJCQkJCWFuaW1hdGVkPSItMCR7YW5pbWF0ZWQ6MX0iCgkJCQlmaQoJCQkJZWNobyAuL2JlbGxhX2NsaSAtaToiJGVhY2giIC1vbjoiJHtwcmVmaXh9JHtwYWRkZWR9IiAtcGY6IiR7QkVMTEFfUEFSU0VfRlJBR01FTlR9IiAtcGY6IiR7YXR0cn09JHthbmltYXRlZH1mOyIgLXBmOiJzZXR0aW5ncy50aHJlYWRzPTA7IiAgLW9kOiIuLyIgCgkJCQkuL2JlbGxhX2NsaSAtaToiJGVhY2giIC1vbjoiJHtwcmVmaXh9JHtwYWRkZWR9IiAtcGY6IiR7QkVMTEFfUEFSU0VfRlJBR01FTlR9IiAtcGY6IiR7YXR0cn09JHthbmltYXRlZH1mOyIgLXBmOiJzZXR0aW5ncy50aHJlYWRzPTA7IiAgLW9kOiIuLyIgPiBiZWxsYS5sb2cKCQkJZG9uZQkKCQllbHNlCgoJCQkuL2JlbGxhX2NsaSAtaToiJHtlYWNofSIgLXBmOiIke0JFTExBX1BBUlNFX0ZSQUdNRU5UfSIgLXBmOiIke2luc2VydDF9IiAtcGY6InNldHRpbmdzLnRocmVhZHM9MDsiIC1vZDoiLi8iID4gYmVsbGEubG9nCgkJZmkKCQlybSAke2VhY2h9Cglkb25lCmZpCg=="
+#cat render.sh | base64 -w0
+render_sh="IyEvYmluL2Jhc2gKb3NfbmFtZT0kKGF3ayAtRj0gJyQxPT0iTkFNRSIgeyBwcmludCAkMiA7fScgL2V0Yy9vcy1yZWxlYXNlKQpwbGF0Zm9ybV9pZD0kKGF3ayAtRj0gJyQxPT0iUExBVEZPUk1fSUQiIHsgcHJpbnQgJDIgO30nIC9ldGMvb3MtcmVsZWFzZSkKdXNlcj0kKHdob2FtaSkKZ3JvdXA9JChpZCAtZykKaG9zdG5hbWU9JChob3N0bmFtZSkKaWYgWyAke2hvc3RuYW1lfSA9PSAiYmVsbGExIiBdOyB0aGVuCgliZWxsYV9iaW49Ii91c3IvbG9jYWwvYmluL2JlbGxhX2NsaSIKZWxzZQoJYmVsbGFfYmluPSIuL2JlbGxhX2NsaSIKCWlmICEgdGVzdCAtZiBiZWxsYV9jbGk7IHRoZW4KCQkjIFJIRUwgOC54IGFuZCA5LnggCgkJaWYgWyAiJHBsYXRmb3JtX2lkIiA9PSAiXCJwbGF0Zm9ybTplbDhcIiIgXSB8fCBbICIkcGxhdGZvcm1faWQiID09ICJcInBsYXRmb3JtOmVsOVwiIiBdOyB0aGVuCgkJCWlmIFsgJHtVU0VSfSA9PSAicm9vdCIgXTsgdGhlbgoJCQkJZG5mIC15IGluc3RhbGwgbWVzYS12dWxrYW4tZHJpdmVycwoJCQkJZG5mIC15IGluc3RhbGwgbWVzYS1saWJHTAoJCQkJZG5mIC15IGluc3RhbGwgYmMKCQkJZWxzZQoJCQkJc3VkbyBkbmYgLXkgaW5zdGFsbCBtZXNhLXZ1bGthbi1kcml2ZXJzCgkJCQlzdWRvIGRuZiAteSBpbnN0YWxsIG1lc2EtbGliR0wKCQkJCXN1ZG8gZG5mIC15IGluc3RhbGwgYmMKCQkJZmkKCQkjICBEZWJpYW4gYmFzZWQKCQllbHNlCgkJCWlmIFsgJHtVU0VSfSA9PSAicm9vdCIgXTsgdGhlbgoJCQkJYXB0IC15IHVwZGF0ZQoJCQkJYXB0IC15IGluc3RhbGwgbWVzYS12dWxrYW4tZHJpdmVycwoJCQkJYXB0IC15IGluc3RhbGwgbGliZ2wxLW1lc2EtZ2x4CgkJCQlhcHQgLXkgaW5zdGFsbCBiYwoJCQllbHNlCgkJCQlzdWRvIGFwdCAteSB1cGRhdGUKCQkJCXN1ZG8gYXB0IC15IGluc3RhbGwgbWVzYS12dWxrYW4tZHJpdmVycwoJCQkJc3VkbyBhcHQgLXkgaW5zdGFsbCBsaWJnbDEtbWVzYS1nbHgKCQkJCXN1ZG8gYXB0IC15IGluc3RhbGwgYmMKCQkJZmkKCQlmaQoJCWlmIFsgLXogJHtCRUxMQV9WRVJTSU9OfSBdOyB0aGVuCgkJCWN1cmwgLU8gaHR0cHM6Ly9kb3dubG9hZHMuYmVsbGFyZW5kZXIuY29tL2JlbGxhX2NsaS0yNC4yLjAudGFyLmd6CgkJCXRhciAteHZmIGJlbGxhX2NsaS0yNC4yLjAudGFyLmd6CgkJZWxzZQoJCQljdXJsIC1PIGh0dHBzOi8vZG93bmxvYWRzLmJlbGxhcmVuZGVyLmNvbS9iZWxsYV9jbGktJHtCRUxMQV9WRVJTSU9OfS50YXIuZ3oKCQkJdGFyIC14dmYgYmVsbGFfY2xpLSR7QkVMTEFfVkVSU0lPTn0udGFyLmd6CgkJZmkKCWZpCmZpCgpic3pfZmlsZXM9Ki5ic3oKCmlmICEgdGVzdCAtZiAiYmVsbGEubG9nIiA7IHRoZW4KCWlkbGU9IjEiCmVsc2UKCWlzX2lkbGU9IiQocGdyZXAgLUcgJHtncm91cH0gYmVsbGFfY2xpKSIKCglpZiBbIC16ICR7aXNfaWRsZX0gXTsgdGhlbgoJCWlkbGU9IjEiCgllbHNlCgkJaWRsZT0iMCIKCWZpCmZpCgppZiBbICR7aWRsZX0gPT0gIjEiIF07IHRoZW4KCWZvciBlYWNoIGluICRic3pfZmlsZXMKCWRvCgkJcHJlZml4PSR7ZWFjaCUuKn0KCQllY2hvICJSZW5kZXJpbmcgc3RhcnRlZCBmb3I6ICRlYWNoIgoJCWlmIHRlc3QgLWYgY2xvdWRfYmVsbGFfY2xpLmFuaW07IHRoZW4KCQkJYW5pbTFzdGFydGxpbmU9JChzZWQgJzEhZCcgY2xvdWRfYmVsbGFfY2xpLmFuaW0pCgkJCWF0dHI9JHthbmltMXN0YXJ0bGluZSU9Kn0KCQkJYW5pbTFzdGFydDA9JHthbmltMXN0YXJ0bGluZSMqPX0KCQkJYW5pbTFzdGFydD0ke2FuaW0xc3RhcnQwOjA6JHsjYW5pbTFzdGFydDB9LTJ9CgkJCWFuaW0xZW5kbGluZT0kKHNlZCAnMiFkJyBjbG91ZF9iZWxsYV9jbGkuYW5pbSkKCQkJYW5pbTFlbmQwPSR7YW5pbTFlbmRsaW5lIyo9fQoJCQlhbmltMWVuZD0ke2FuaW0xZW5kMDowOiR7I2FuaW0xZW5kMH0tMn0KCQkJZnJhbWVzPSQoc2VkICczIWQnIGNsb3VkX2JlbGxhX2NsaS5hbmltKQoJCQlmb3IgKChpID0gMCA7IGkgPD0gJHtmcmFtZXN9IDsgaSsrICkpOyBkbyAKCQkJCXBhZGRlZD0kKHByaW50ZiAiJTA0ZCIgJCgoaSsxKSkpCgkJCQllY2hvICJzY2FsZT01OyAoJHthbmltMWVuZH0tKCR7YW5pbTFzdGFydH0pKSooJHtpfS8ke2ZyYW1lc30uMCkrJHthbmltMXN0YXJ0fSIgfCBiYwoJCQkJYW5pbWF0ZWQ9JChlY2hvICJzY2FsZT01OyAoKCR7YW5pbTFlbmR9LSgke2FuaW0xc3RhcnR9KSkqKCR7aX0vJHtmcmFtZXN9LjApKSske2FuaW0xc3RhcnR9IiB8IGJjKQoJCQkJaWYgWyAke2FuaW1hdGVkOjA6MX0gPT0gIi4iIF07IHRoZW4KCQkJCQlhbmltYXRlZD0iMCR7YW5pbWF0ZWR9IgoJCQkJZWxpZiBbICR7YW5pbWF0ZWQ6MDoyfSA9PSAiLS4iIF07IHRoZW4KCQkJCQlhbmltYXRlZD0iLTAke2FuaW1hdGVkOjF9IgoJCQkJZmkKCQkJCWVjaG8gJHtiZWxsYV9iaW59IC1pOiIkZWFjaCIgLW9uOiIke3ByZWZpeH0ke3BhZGRlZH0iIC1wZjoiJHtCRUxMQV9QQVJTRV9GUkFHTUVOVH0iIC1wZjoiJHthdHRyfT0ke2FuaW1hdGVkfWY7IiAtcGY6InNldHRpbmdzLnRocmVhZHM9MDsiICAtb2Q6Ii4vIiAKCQkJCSR7YmVsbGFfYmlufSAtaToiJGVhY2giIC1vbjoiJHtwcmVmaXh9JHtwYWRkZWR9IiAtcGY6IiR7QkVMTEFfUEFSU0VfRlJBR01FTlR9IiAtcGY6IiR7YXR0cn09JHthbmltYXRlZH1mOyIgLXBmOiJzZXR0aW5ncy50aHJlYWRzPTA7IiAgLW9kOiIuLyIgPiBiZWxsYS5sb2cKCQkJZG9uZQkKCQllbHNlCgoJCQkke2JlbGxhX2Jpbn0gLWk6IiR7ZWFjaH0iIC1wZjoiJHtCRUxMQV9QQVJTRV9GUkFHTUVOVH0iIC1wZjoiJHtpbnNlcnQxfSIgLXBmOiJzZXR0aW5ncy50aHJlYWRzPTA7IiAtb2Q6Ii4vIiA+IGJlbGxhLmxvZwoJCWZpCgkJcm0gJHtlYWNofQoJZG9uZQplbHNlCgllY2hvICJiZWxsYV9jbGkgaXMgYWxyZWFkeSBydW5uaW5nLCBub3RoaW5nIG5ldyBpcyBiZWluZyBkb25lIgpmaQo="
 
 #echo ${render_sh} | base64 -d -i - > render.sh
 
@@ -35,17 +35,13 @@ do
 			echo -e "\nLinux server internet address"
 			read -p "Enter x.x.x.x:" server_ip
 			if ! [ -z ${server_ip} ]; then
-				ssh -o PasswordAuthentication=no -o PubkeyAuthentication=yes root@${server_ip} "exit" 
-				PUBKEY_RESULT=$?
-				echo $PUBKEY_RESULT
-				if ! [ $PUBKEY_RESULT -eq 0 ]; then
-					echo -e "\nYour Linux server does not expect the username root"
-					echo -e "\nRemember to edit the username in the server menu to match your provider"
-					echo -e "\nOtherwise this script will fail to render"
-				fi
 				break
 			fi
 		done
+		if [ ${server_ip} == "masey.indoodle.com" ]; then
+			server_user="oomer"
+			server_port="222"
+		fi
 		
 	else
 		echo -e "\n================================================="	
@@ -71,6 +67,8 @@ do
 			vast_host=$(ssh ${server_user}@${server_ip} -p ${server_port} test -f .vast_containerlabel; echo $?)
 			if [ ${server_user} == "bella" ]; then
 				echo "bypass"
+			elif [ ${server_ip} == "masey.indoodle.com" ]; then
+				echo "bypass"
 			elif [ ${server_user} == "root" ]; then
 				ssh ${server_user}@${server_ip} -p ${server_port} "sed -i /'AcceptEnv LANG LC_*'/d /etc/ssh/sshd_config"
 				ssh ${server_user}@${server_ip} -p ${server_port} "grep -qxF 'AcceptEnv BELLA_LICENSE_TEXT' /etc/ssh/sshd_config || echo 'AcceptEnv BELLA_LICENSE_TEXT' >> /etc/ssh/sshd_config"
@@ -83,9 +81,9 @@ do
 					ssh ${server_user}@${server_ip} -p ${server_port} "/etc/init.d/ssh restart"
 				fi
 			else
-				ssh ${server_user}@${server_ip} -p ${server_port} "sudo grep -qxF 'AcceptEnv BELLA_LICENSE_TEXT' /etc/ssh/sshd_config || echo 'AcceptEnv BELLA_LICENSE_TEXT' | sudo tee -a /etc/ssh/sshd_config"
-				ssh ${server_user}@${server_ip} -p ${server_port} "sudo grep -qxF 'AcceptEnv BELLA_PARSE_FRAGMENT' /etc/ssh/sshd_config || echo 'AcceptEnv BELLA_PARSE_FRAGMENT' | sudo tee -a /etc/ssh/sshd_config"
-				ssh ${server_user}@${server_ip} -p ${server_port} "sudo grep -qxF 'AcceptEnv BELLA_VERSION' /etc/ssh/sshd_config || echo 'AcceptEnv BELLA_VERSION' | sudo tee -a /etc/ssh/sshd_config"
+				ssh ${server_user}@${server_ip} -p ${server_port} "grep -qxF 'AcceptEnv BELLA_LICENSE_TEXT' /etc/ssh/sshd_config || echo 'AcceptEnv BELLA_LICENSE_TEXT' | sudo tee -a /etc/ssh/sshd_config"
+				ssh ${server_user}@${server_ip} -p ${server_port} "grep -qxF 'AcceptEnv BELLA_PARSE_FRAGMENT' /etc/ssh/sshd_config || echo 'AcceptEnv BELLA_PARSE_FRAGMENT' | sudo tee -a /etc/ssh/sshd_config"
+				ssh ${server_user}@${server_ip} -p ${server_port} "grep -qxF 'AcceptEnv BELLA_VERSION' /etc/ssh/sshd_config || echo 'AcceptEnv BELLA_VERSION' | sudo tee -a /etc/ssh/sshd_config"
 				echo -e "\n${vast_host}"
 				if [ ${vast_host} == "1" ]; then
 					ssh ${server_user}@${server_ip} -p ${server_port} "sudo systemctl restart sshd"
@@ -107,8 +105,7 @@ do
 
 			ssh ${server_user}@${server_ip} -p ${server_port} "echo ${render_sh} | base64 --decode > render.sh"
 			ssh ${server_user}@${server_ip} -p ${server_port} "echo ${pf64} | base64 --decode > pf.txt"
-			ssh ${server_user}@${server_ip} -o SendEnv=BELLA_LICENSE_TEXT -p ${server_port} "nohup bash render.sh >./nohup.out 2>./nohup.err &"
-			echo "The render is running on your Linux server in the background. Occasionally use the 3) progress option and look for the word DONE. You can close this window. Remember to shut down any hourly cloud computers you are running to avoid unexpected charges"
+			ssh ${server_user}@${server_ip} -p ${server_port} "nohup bash render.sh >./nohup.out 2>./nohup.err &"
 		elif [[ $action == "server" ]]; then
 			select setting in set_server set_port set_user systemd delete_files delete_bella_cli stop_bella back
 			do
